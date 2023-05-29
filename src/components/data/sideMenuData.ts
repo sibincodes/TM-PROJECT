4e4ec41d81f27bf4cffb1a412b7f11448cc67673
Gowri Lakshmi D <gowri@terrificminds.com>
2023-05-29 17:56:25 +0530

    side nav basic structure

---
+++ src/components/data/sideMenuData.ts
@@ -0,0 +1,64 @@
+export const sideMenuOptions=[{
+    mainMenu:'set up',
+    childItems:[{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+]
+},
+{
+    mainMenu:'set up',
+    childItems:[{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+{
+    icon:'',
+    title:'Leave Approval'
+},
+]
+}]
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,17 @@
 
+import { ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home/home";
+import { theme } from "./ThemeProvider";
 
 export const RoutesPage = () => {
   return (
     <>
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      </ThemeProvider>
     </>
   );
 };
