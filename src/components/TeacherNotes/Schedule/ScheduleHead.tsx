3fd2fcd27b435ed22e8c257e6fe789d71c974ce3
Sibin <sibinjohn@terrificminds.com>
2023-06-09 15:26:28 +0530

    calendar view date formatted

--- src/components/TeacherNotes/Schedule/ScheduleHead.tsx
+++ src/components/TeacherNotes/Schedule/ScheduleHead.tsx
@@ -2,16 +2,19 @@ import { Button } from "@mui/material";
 import { ReactSVG } from "react-svg";
 import { ScheduleHead } from "../../../styles/scheduleHead";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DateCalendarValue from "./ScheduleCalendar";
 import dayjs, { Dayjs } from "dayjs";
 import { Link } from "react-router-dom";
+import dateFormat from 'dateformat';
 
 const ScheduleHeadComponent = () => {
   const icons = <ReactSVG src="/HeaderIcons/next.svg" />;
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState<Dayjs | null>(dayjs("2022-04-17"));
-
+useEffect(()=>{
+console.log("value",dateFormat(value?.toDate(), "mmmm dS, yyyy"));
+},[value]);
   return (
     <ScheduleHead>
       <h4>Schedule</h4>
